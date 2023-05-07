466525fbb11b0402b2ec4733091b770f88af0020
Yuri <yuriscoutinho999@gmail.com>
2023-05-07 14:23:17 -0300

    feat: add mobile layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import css from './sass/main.scss'
+import css from "./sass/main.scss";
+const introduction = document.querySelector(".introduction");
+const header = document.querySelector(".header");
+const headerTitle = document.querySelector(".header__title");
+
+window.addEventListener("scroll", () => {
+  let introductionPosition = introduction.getBoundingClientRect().top;
+  if (introductionPosition != 0) {
+    header.classList.add("header--inverted");
+    headerTitle.classList.add("header__title--inverted");
+    headerButton.classList.add("header__button--inverted");
+  } else {
+    header.classList.remove("header--inverted");
+    headerTitle.classList.remove("header__title--inverted");
+    headerButton.classList.remove("header__button--inverted");
+  }
+});
